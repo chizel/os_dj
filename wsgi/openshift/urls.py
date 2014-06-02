4b55d97f8805eec118485ab875cd989f12040ec6
Denis Gatin <chizel@inbox.ru>
2014-06-02 17:11:13 +0300

    move projects to openshift

--- wsgi/openshift/urls.py
+++ wsgi/openshift/urls.py
@@ -1,12 +1,19 @@
 from django.conf.urls import patterns, include, url
+from django.conf import settings
+from django.conf.urls.static import static
 
 from django.contrib import admin
+
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    # Examples:
-    url(r'^$', 'views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+urlpatterns = [
 
+    #url(r'^$', include('forum.urls')),
+    url(r'^$', 'views.home', name='home'),
     url(r'^admin/', include(admin.site.urls)),
-)
+
+    url('', include('social.apps.django_app.urls', namespace='social')),
+    url(r'^forum/', include('forum.urls', namespace='forum')),
+    url(r'^user/', include('userprofile.urls', namespace='userprofile')),
+    url(r'^blog/', include('blog.urls', namespace='blog')),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
