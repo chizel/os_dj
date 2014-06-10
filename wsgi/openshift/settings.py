4ca16c51c71950c868819381b5d194980fed46e9
Denis Gatin <chizel@inbox.ru>
2014-06-10 09:05:31 +0300

    change social_auth_pipeline

--- wsgi/openshift/settings.py
+++ wsgi/openshift/settings.py
@@ -144,12 +144,15 @@ INSTALLED_APPS = (
 
 #SOCIAL AUTHORIZATION
 SOCIAL_AUTH_PIPELINE = (
-'social_auth.backends.pipeline.social.social_auth_user',
-'social_auth.backends.pipeline.associate.associate_by_email',
-'social_auth.backends.pipeline.user.get_username',
-'social_auth.backends.pipeline.user.create_user',
-'social_auth.backends.pipeline.social.associate_user',
-'social_auth.backends.pipeline.user.update_user_details',
+'social.pipeline.social_auth.social_details',
+'social.pipeline.social_auth.social_uid',
+'social.pipeline.social_auth.auth_allowed',
+'social.pipeline.social_auth.social_user',
+'social.pipeline.user.get_username',
+'social.pipeline.user.create_user',
+'social.pipeline.social_auth.associate_user',
+'social.pipeline.social_auth.load_extra_data',
+'social.pipeline.user.user_details'
 'userprofile.pipelines.user_details',
 )
 
