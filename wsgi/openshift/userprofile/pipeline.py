5476d2e95a51dd77c23315c7875ddae8c9e6b447
Denis Gatin <chizel@inbox.ru>
2014-06-21 20:36:40 +0300

    try to get avatar from twitter account

--- wsgi/openshift/userprofile/pipeline.py
+++ wsgi/openshift/userprofile/pipeline.py
@@ -6,21 +6,9 @@ def user_details(strategy, details, response, is_new=False, user=None, *args, **
     """Update user details using data from provider."""
     if user:
         if is_new:
-            attrs = {'user': user}
 
             if strategy.backend.__class__.__name__ == 'TwitterOAuth':
                 #twitter_data = {}
-                #attrs = dict(attrs.items() + twitter_data.items())
-
-                #UserProfile.objects.create(
-                    #**attrs
-                #)
-
-                profile = UserProfile()
-                profile.user = user
-                profile.save()
-                return
-
             elif strategy.backend.__class__.__name__ == 'FacebookOAuth2':
                 # We should check values before this, but for the example
                 # is fine
@@ -30,33 +18,38 @@ def user_details(strategy, details, response, is_new=False, user=None, *args, **
                 #'locale': response['locale'],
                 #'dob': datetime.fromtimestamp(mktime(strptime(response['birthday'], '%m/%d/%Y')))
                 #}
-                #attrs = dict(attrs.items() + fb_data.items())
-                #UserProfile.objects.create(
-                #**attrs
-                #)
-                profile = UserProfile()
-                profile.user = user
-                profile.save()
             elif strategy.backend.__class__.__name__ == 'GithubOAuth2':
-                profile = UserProfile()
-                profile.user = user
-                profile.save()
+                pass
+            profile = UserProfile()
+            profile.user = user
+            profile.picture = get_user_avatar()
+            profile.save()
+        profile.picture = get_user_avatar()
+        profile.save()
 
   
-def get_user_avatar(backend, details, response, social_user, uid, user, *args, **kwargs):
-    url = None
-    if backend.__class__ == FacebookBackend:
-        url = "http://graph.facebook.com/%s/picture?type=large" % response['id']
+def get_user_avatar(strategy, details, response, social_user, uid, user, *args, **kwargs):
+    #if not "id" in response:
+        return None
 
-    elif backend.__class__ == TwitterBackend:
+    url = None
+
+    if strategy.backend.__class__.__name__ == 'FacebookOAuth2':
+        url = "http://graph.facebook.com/%s/picture?type=large" % response['id']
+    elif strategy.backend.__class__.__name__ == 'TwitterOAuth':
         #!!! check is user uploaded avatar: default_profile_image 
         url = response.get('profile_image_url', '').replace('_normal', '')
+        #url = response["profile_image_url"]
+    elif strategy.backend.__class__.__name__ == 'GithubOAuth2':
+        pass
 
     if url:
         profile = user.get_profile()
         avatar = urlopen(url).read()
-        fout = open(filepath, "wb")#filepath is where to save the image
-        fout.write(avatar)
-        fout.close()
-        profile.photo = url_to_image # depends on where you saved it
-        profile.save()
+        #fout = open(filepath, "wb")
+        #fout.write(avatar)
+        #fout.close()
+        #profile.picture = url_to_image # depends on where you saved it
+        #profile.save()
+        return avatar
+    return None
