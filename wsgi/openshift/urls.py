c4d3362533397d17cc3b361e57a4318af30e9138
Denis <prohibiti@gmail.com>
2015-01-27 18:03:34 +0200

    correct some mistakes

--- wsgi/openshift/urls.py
+++ wsgi/openshift/urls.py
@@ -12,7 +12,6 @@ urlpatterns =[
     url(r'^admin/', include(admin.site.urls)),
     url('', include('social.apps.django_app.urls', namespace='social')),
     url('', include('django.contrib.auth.urls', namespace='auth')), 
-    url(r'^tinymce/', include('tinymce.urls')),
 
     url(r'^forum/', include('forum.urls', namespace='forum')),
     url(r'^user/', include('userprofile.urls', namespace='userprofile')),
