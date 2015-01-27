b043151040125365f263e8a1fdbc09df260163f6
Denis <prohibiti@gmail.com>
2015-01-27 18:55:37 +0200

    set

--- wsgi/openshift/wsgi.py
+++ wsgi/openshift/wsgi.py
@@ -8,8 +8,10 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import os
+import django
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+django.setup()
 
 from django.core.wsgi import get_wsgi_application
 
